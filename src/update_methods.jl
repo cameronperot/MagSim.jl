05e3d4e43a28ab73bb883da25e3b2dd402560a68
Cameron Perot <cameronperot@users.noreply.github.com>
2020-03-03 17:06:53 -0600

    Updated method for computing observables statistics

--- src/update_methods.jl
+++ src/update_methods.jl
@@ -90,42 +90,31 @@ end
 
 
 """
-	compute_observables_statistics!(model::Union{Ising, Potts})
+	compute_observables_statistics!(model::AbstractMagnetismModel)
 
 Computes the average energy per spin, average magnetization per spin,
 heat capacity per spin, magnetic susceptibility per spin, Binder parameter,
-and the integrated autocorrelation time (computed using the binning method).
+and the integrated autocorrelation times (computed using the binning method).
 """
-function compute_observables_statistics!(model::Union{Ising, Potts})
+function compute_observables_statistics!(model::AbstractMagnetismModel)
 	V = model.params.L^2
 	model.observables.statistics[:e]  = mean(model.observables.E) / V
 	model.observables.statistics[:e²] = mean(model.observables.E.^2) / V^2
-	model.observables.statistics[:m]  = mean(abs.(model.observables.M)) / V
-	model.observables.statistics[:m²] = mean(model.observables.M.^2) / V^2
 	model.observables.statistics[:c]  = model.params.β^2 * var(model.observables.E) / V
-	model.observables.statistics[:χ]  = model.params.β * var(abs.(model.observables.M)) / V
-	model.observables.statistics[:U]  = 1 - mean(model.observables.M.^4) / 3 / mean(model.observables.M.^2)^2
-	model.observables.statistics[:τ_int] = compute_autocorrelation_time(model, collect(1:400))
-end
 
+	if isa(model, Ising)
+		model.observables.statistics[:m]  = mean(abs.(model.observables.M)) / V
+		model.observables.statistics[:m²] = mean(model.observables.M.^2) / V^2
+		model.observables.statistics[:χ]  = model.params.β * var(abs.(model.observables.M)) / V
+		model.observables.statistics[:U]  = 1 - mean(model.observables.M.^4) / 3 / mean(model.observables.M.^2)^2
+	elseif isa(model, XY)
+		model.observables.statistics[:mx]  = mean(model.observables.Mx) / V
+		model.observables.statistics[:mx²] = mean(model.observables.Mx.^2) / V^2
+		model.observables.statistics[:my]  = mean(model.observables.My) / V
+		model.observables.statistics[:my²] = mean(model.observables.My.^2) / V^2
+		model.observables.statistics[:m²]  = mean(model.observables.Mx.^2 .+ model.observables.My.^2) / V^2
+		model.observables.statistics[:χ]   = model.observables.statistics[:m²] * V
+	end
 
-"""
-	compute_observables_statistics!(model::XY)
-
-Computes the average energy per spin, average magnetization per spin,
-heat capacity per spin, magnetic susceptibility per spin,
-and the integrated autocorrelation time (computed using the binning method).
-"""
-function compute_observables_statistics!(model::XY)
-	V = model.params.L^2
-	model.observables.statistics[:e]   = mean(model.observables.E) / V
-	model.observables.statistics[:e²]  = mean(model.observables.E.^2) / V^2
-	model.observables.statistics[:mx]  = mean(model.observables.Mx) / V
-	model.observables.statistics[:mx²] = mean(model.observables.Mx.^2) / V^2
-	model.observables.statistics[:my]  = mean(model.observables.My) / V
-	model.observables.statistics[:my²] = mean(model.observables.My.^2) / V^2
-	model.observables.statistics[:m²]  = mean(model.observables.Mx.^2 .+ model.observables.My.^2) / V^2
-	model.observables.statistics[:c]   = model.params.β^2 * var(model.observables.E) / V
-	model.observables.statistics[:χ]   = V * model.observables.statistics[:m²]
 	model.observables.statistics[:τ_int] = compute_autocorrelation_time(model, collect(1:400))
 end
