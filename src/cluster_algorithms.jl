d3c6ab3874a91dffd4576f3d4bd858b659c5ce22
Cameron Perot <cameronperot@users.noreply.github.com>
2020-03-03 01:08:02 -0600

    Added Wolff algorithm for Potts model

--- src/cluster_algorithms.jl
+++ src/cluster_algorithms.jl
@@ -47,7 +47,44 @@ end
 	wolff!(model::Potts)
 
 """
-function wolff!(model::Potts)
+function wolff!(model::Potts, avg_cluster_size::Int)
+	P          = 1 - exp(-model.params.β)
+	n_clusters = Int(ceil(model.params.L^2 / avg_cluster_size))
+	t₀         = floor(Int, model.params.cutoff * model.params.n_sweeps)
+	indices    = 1:length(model.σ)
+
+	for t in 1:model.params.n_sweeps
+		for n in 1:n_clusters
+			idx           = CartesianIndices(model.σ)[rand(model.rng, indices)]
+			i, j          = idx[1], idx[2]
+			old_spin      = model.σ[i, j]
+			new_spin      = rand(model.rng, UnitRange{Int8}(1, model.params.q))
+			model.σ[i, j] = new_spin
+			counts        = compute_counts(model, i, j)
+			model.observables.E_current += counts[old_spin] - counts[new_spin]
+
+			cluster = Tuple{Int, Int}[(i, j)]
+			for (i, j) in cluster
+				neighbors = get_neighbor_indices(model, i, j)
+				for (k, l) in neighbors
+					if model.σ[k, l] == old_spin && rand(model.rng) < P
+						model.σ[k, l] = new_spin
+						push!(cluster, (k, l))
+						counts = compute_counts(model, k, l)
+						model.observables.E_current += counts[old_spin] - counts[new_spin]
+					end
+				end
+			end
+
+			model.observables.avg_cluster_size += length(cluster)
+		end
+
+		t > t₀ && update_observables!(model)
+	end
+
+	model.observables.avg_cluster_size /= (model.params.n_sweeps * n_clusters)
+	compute_observables_statistics!(model)
+	return model
 end
 
 
