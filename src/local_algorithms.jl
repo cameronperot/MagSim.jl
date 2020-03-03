533522fd08e8d8876c88c7d0059a50ae85a66580
Cameron Perot <cameronperot@users.noreply.github.com>
2020-03-02 19:28:47 -0600

    Added the Metropolis and heat bath methods for XY model

--- src/local_algorithms.jl
+++ src/local_algorithms.jl
@@ -1,7 +1,7 @@
 """
 	metropolis!(model::Ising)
 
-Implementation of the famous Metropolis local update algorithm for the Ising model.
+Implementation of the Metropolis local update algorithm for the Ising model.
 """
 function metropolis!(model::Ising)
 	t₀   = floor(Int, model.params.cutoff * model.params.n_sweeps)
@@ -19,7 +19,7 @@ function metropolis!(model::Ising)
 			end
 		end
 
-		t > t₀ && update_observables!(model, t)
+		t > t₀ && update_observables!(model)
 	end
 
 	compute_observables_statistics!(model)
@@ -30,11 +30,11 @@ end
 """
 	metropolis!(model::Potts)
 
-Implementation of the famous Metropolis local update algorithm for the Potts model.
+Implementation of the Metropolis local update algorithm for the Potts model.
 """
 function metropolis!(model::Potts)
-	t₀   = floor(Int, model.params.cutoff * model.params.n_sweeps)
-	expᵦ = exp(-model.params.β)
+	t₀ = floor(Int, model.params.cutoff * model.params.n_sweeps)
+	eᵝ = exp(-model.params.β)
 
 	for t in 1:model.params.n_sweeps
 		for j in 1:model.params.L, i in 1:model.params.L
@@ -43,13 +43,13 @@ function metropolis!(model::Potts)
 			new_spin = rand(model.rng, UnitRange{Int8}(1, model.params.q))
 			ΔE       = counts[old_spin] - counts[new_spin]
 
-			if ΔE <= 0 || rand(model.rng) < expᵦ^ΔE
+			if ΔE <= 0 || rand(model.rng) < eᵝ^ΔE
 				model.σ[i, j] = new_spin
 				model.observables.E_current += ΔE
 			end
 		end
 
-		t > t₀ && update_observables!(model, t)
+		t > t₀ && update_observables!(model)
 	end
 
 	compute_observables_statistics!(model)
@@ -60,8 +60,31 @@ end
 """
 	metropolis!(model::XY)
 
+Implementation of the Metropolis local update algorithm for the XY model.
 """
 function metropolis!(model::XY)
+	t₀ = floor(Int, model.params.cutoff * model.params.n_sweeps)
+	eᵝ = exp(-model.params.β)
+
+	for t in 1:model.params.n_sweeps
+		for j in 1:model.params.L, i in 1:model.params.L
+			old_spin = model.σ[i, j]
+			new_spin = random_XYVector(model.rng)
+			ΔE       = dot(old_spin .- new_spin, sum_of_neighbors(model, i, j))
+
+			if ΔE <= 0 || rand(model.rng) < eᵝ^ΔE
+				model.σ[i, j] = new_spin
+				model.observables.E_current  += ΔE
+				model.observables.Mx_current += new_spin[1] - old_spin[1]
+				model.observables.My_current += new_spin[2] - old_spin[2]
+			end
+		end
+
+		t > t₀ && update_observables!(model)
+	end
+
+	compute_observables_statistics!(model)
+	return model
 end
 
 
@@ -78,16 +101,15 @@ function heat_bath!(model::Ising)
 	for t in 1:model.params.n_sweeps
 		for j in 1:model.params.L, i in 1:model.params.L
 			k = Int(model.σ[i, j] * sum_of_neighbors(model, i, j) / 2 + 3)
-			P = exps[k] / (exps[k] + 1)
 
-			if rand(model.rng) < P
+			if rand(model.rng) < exps[k] / (exps[k] + 1)
 				model.σ[i, j] *= -1
 				model.observables.E_current += ΔEs[k]
 				model.observables.M_current += 2 * model.σ[i, j]
 			end
 		end
 
-		t > t₀ && update_observables!(model, t)
+		t > t₀ && update_observables!(model)
 	end
 
 	compute_observables_statistics!(model)
@@ -122,7 +144,7 @@ function heat_bath!(model::Potts)
 			end
 		end
 
-		t > t₀ && update_observables!(model, t)
+		t > t₀ && update_observables!(model)
 	end
 
 	compute_observables_statistics!(model)
@@ -133,8 +155,31 @@ end
 """
 	heat_bath!(model::XY)
 
+Implementation of the Metropolis local update algorithm for the XY model.
 """
 function heat_bath!(model::XY)
+	t₀ = floor(Int, model.params.cutoff * model.params.n_sweeps)
+	eᵝ = exp(-model.params.β)
+
+	for t in 1:model.params.n_sweeps
+		for j in 1:model.params.L, i in 1:model.params.L
+			old_spin = model.σ[i, j]
+			new_spin = random_XYVector(model.rng)
+			ΔE       = dot(old_spin .- new_spin, sum_of_neighbors(model, i, j))
+
+			if rand(model.rng) < eᵝ^ΔE / (eᵝ^ΔE + 1)
+				model.σ[i, j] = new_spin
+				model.observables.E_current  += ΔE
+				model.observables.Mx_current += new_spin[1] - old_spin[1]
+				model.observables.My_current += new_spin[2] - old_spin[2]
+			end
+		end
+
+		t > t₀ && update_observables!(model)
+	end
+
+	compute_observables_statistics!(model)
+	return model
 end
 
 
@@ -154,8 +199,9 @@ end
 
 
 """
-	compute_counts(q::Int, σ::Array{Int8, 1}, i::Int, j::Int)
+	compute_counts(model::Potts, i::Int, j::Int)
 
+Computes the counts/frequency of the different spin vaues neighboring `σ[i, j]`.
 """
 function compute_counts(model::Potts, i::Int, j::Int)
 	counts = zeros(Int8, model.params.q)
@@ -166,4 +212,19 @@ function compute_counts(model::Potts, i::Int, j::Int)
 	counts[model.σ[i, minus(j, model.params.L)]] += 1
 
 	return counts
+end
+
+
+"""
+	sum_of_neighbors(model::XY, i::Int, j::Int)
+
+Sums the values of the neighbors for a given site (i, j).
+"""
+function sum_of_neighbors(model::XY, i::Int, j::Int)
+	return (
+		model.σ[plus(i, model.params.L), j] .+
+		model.σ[minus(i, model.params.L), j] .+
+		model.σ[i, plus(j, model.params.L)] .+
+		model.σ[i, minus(j, model.params.L)]
+		)
 end
